4d42a27ab562a47291acb5812c656959bcb783c5
vanderson Teles <vander.drums@hotmail.com>
2023-08-15 23:34:50 -0300

    adição de classes relacionadas ao SpringSecurity

---
+++ src/main/java/med/voll/api/infra/security/TokenServiceAPI.java
@@ -0,0 +1,52 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+@Service
+public class TokenServiceAPI {
+@Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario usuario){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException e){
+            throw new RuntimeException("erro na geração do token" , e);
+        }
+
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            throw  new RuntimeException("Token invalido ou expirado", e);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+
+}
